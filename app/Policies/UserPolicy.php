0e9daaf3439bad1a35a840194faff943a342a245
Azizi <azizishaharuddin@gmail.com>
2021-11-08 07:21:48 +0800

    Update UserPolicy.php

--- app/Policies/UserPolicy.php
+++ app/Policies/UserPolicy.php
@@ -69,6 +69,9 @@ class UserPolicy
     public function delete(User $user, User $model)
     {
         //
+        return $user->role == 1
+        ? Response::allow()
+        : Response::deny('You cannot delete this user');
     }
 
     /**
